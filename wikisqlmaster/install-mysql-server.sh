89a388fa1807e4a9a452479b7c791403161fd8fc
Kirillov Anton <anton.kirillov@gmail.com>
2024-05-13 23:29:07 +0300

    add sql master conf

--- wikisqlmaster/install-mysql-server.sh
+++ wikisqlmaster/install-mysql-server.sh
@@ -7,4 +7,14 @@ SCRIPT_USER="${SUDO_USER:-$USER}"
 CURRENT_IP=$(ip addr | grep 'state UP' -A4 | grep 'inet ' | awk '{print $2}' | cut -f1  -d'/')
 
 # Generate a password for the database
-DB_PASS="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13)"
+DB_PASS="r9PFbhQQ7kwVA"
+
+
+# Set up database
+function run_database_setup() {
+  # Create the required user database, user and permissions in the database
+  mysql -u root --execute="CREATE USER repl@'%' IDENTIFIED WITH 'caching_sha2_password' BY 'oTUSlave#2020';"
+  mysql -u root --execute="GRANT REPLICATION SLAVE ON *.* TO repl@'%';"
+}
+
+run_database_setup 
