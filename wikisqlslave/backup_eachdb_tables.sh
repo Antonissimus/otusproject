b83a3780a0537efc2533aca2bb0027937af84728
Barmaley <anton.kirillov@gmail.com>
2024-05-18 00:04:09 +0300

    добавил бэкап всего инстанса

--- wikisqlslave/backup_eachdb_tables.sh
+++ wikisqlslave/backup_eachdb_tables.sh
@@ -3,16 +3,16 @@
 PATH=$PATH:/usr/local/bin
 
 DIR=`date +"%Y-%m-%d"`
-DATE=`date +"%Y%m%d"`
+DATE=`date +"%Y%m%d-%H%M"`
 MYSQL='mysql --skip-column-names'
 
 `mysql -e "STOP REPLICA"`;
 for d in `$MYSQL -e "SHOW DATABASES"`;
 do
-    mkdir sql_backup/$d;
+    mkdir sql_backup/"${d}_${DATE}";
     for t in `$MYSQL -e "SHOW TABLES from $d"`;
     do
-    /usr/bin/mysqldump --add-drop-table --add-locks --create-options --disable-keys --extended-insert --single-transaction --quick --set-charset --events --routines --triggers $d --tables $t| gzip -1 > sql_backup/$d/$t.gz;
+    /usr/bin/mysqldump --add-drop-table --add-locks --create-options --disable-keys --extended-insert --single-transaction --quick --set-charset --events --routines --triggers $d --tables $t| gzip -1 > sql_backup/"${d}_${DATE}"/$t.gz;
     done
 done
 `mysql -e "START REPLICA"`;
