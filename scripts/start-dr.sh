191dac1a610fba5fbaccc2100c5541caddddf87b
Kirillov Anton <anton.kirillov@gmail.com>
2024-06-28 22:37:44 +0300

    Add packages install

--- scripts/start-dr.sh
+++ scripts/start-dr.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 # Restore wikiweb
+echo устанавливаем nginx
+ssh -i ~/.ssh/id_rsa_root root@wikiweb "apt install nginx"
 
 echo переносим конфигурацию nginx
 scp -i ~/.ssh/id_rsa_root /home/anton/otusproject/wikiweb/nginx/default root@wikiweb:/etc/nginx/sites-available/default
 ssh -i ~/.ssh/id_rsa_root root@wikiweb "systemctl restart nginx"
+
+echo устанавливаем node-exporter
+ssh -i ~/.ssh/id_rsa_root root@wikiweb "curl -LO https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz"
+ssh -i ~/.ssh/id_rsa_root root@wikiweb "tar xzvf node_exporter-*.t*gz"
+ssh -i ~/.ssh/id_rsa_root root@wikiweb "useradd --no-create-home --shell /bin/false node_exporter"
+ssh -i ~/.ssh/id_rsa_root root@wikiweb "cp -r node_exporter-*.linux-amd64/node_exporter /usr/local/bin"
+ssh -i ~/.ssh/id_rsa_root root@wikiweb "chown node_exporter: /usr/local/bin/node_exporter"
 
 echo переносим конфигурацию node-exporter
 scp -i ~/.ssh/id_rsa_root /home/anton/otusproject/wikiweb/node-exporter/node_exporter.service root@wikiweb:/etc/systemd/system/node_exporter.service
@@ -12,12 +21,20 @@ ssh -i ~/.ssh/id_rsa_root root@wikiweb "systemctl daemon-reload"
 ssh -i ~/.ssh/id_rsa_root root@wikiweb "systemctl start node_exporter"
 ssh -i ~/.ssh/id_rsa_root root@wikiweb "systemctl enable node_exporter"
 
+echo устанавливаем filebeat
+scp -i ~/.ssh/id_rsa_root /home/anton/Загрузки/filebeat_8.9.1_amd64-224190-507082.deb root@wikiweb:/home/anton/filebeat_8.9.1_amd64.deb
+ssh -i ~/.ssh/id_rsa_root root@wikiweb "dpkg -i /home/anton/filebeat_8.9.1_amd64.deb"
+
 echo переносим конфигурацию filebeat
 scp -i ~/.ssh/id_rsa_root /home/anton/otusproject/wikiweb/filebeat/filebeat.yml root@wikiweb:/etc/filebeat/filebeat.yml
 ssh -i ~/.ssh/id_rsa_root root@wikiweb "systemctl restart filebeat"
 ssh -i ~/.ssh/id_rsa_root root@wikiweb "systemctl enable filebeat"
 
 # Restore wikibackprimary
+echo устанавливаем bookstack
+scp -i ~/.ssh/id_rsa_root /home/anton/otusproject/scripts/installation-ubuntu-24.04.sh root@wikibackprimary:/home/anton/installation-ubuntu-24.04.sh
+ssh -i ~/.ssh/id_rsa_root root@wikibackprimary "/home/anton/installation-ubuntu-24.04.sh"
+
 echo переносим конфигурацию apache
 scp -i ~/.ssh/id_rsa_root /home/anton/otusproject/wikibackprimary/apache/bookstack.conf root@wikibackprimary:/etc/apache2/sites-available/bookstack.conf
 ssh -i ~/.ssh/id_rsa_root root@wikibackprimary "systemctl restart apache2"
@@ -25,11 +42,22 @@ ssh -i ~/.ssh/id_rsa_root root@wikibackprimary "systemctl restart apache2"
 echo переносим конфигурацию bookstack
 scp -i ~/.ssh/id_rsa_root /home/anton/otusproject/wikibackprimary/bookstack/.env root@wikibackprimary:/var/www/bookstack/.env
 
+echo устанавливаем node-exporter
+ssh -i ~/.ssh/id_rsa_root root@wikibackprimary "curl -LO https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz"
+ssh -i ~/.ssh/id_rsa_root root@wikibackprimary "tar xzvf node_exporter-*.t*gz"
+ssh -i ~/.ssh/id_rsa_root root@wikibackprimary "useradd --no-create-home --shell /bin/false node_exporter"
+ssh -i ~/.ssh/id_rsa_root root@wikibackprimary "cp -r node_exporter-*.linux-amd64/node_exporter /usr/local/bin"
+ssh -i ~/.ssh/id_rsa_root root@wikibackprimary "chown node_exporter: /usr/local/bin/node_exporter"
+
 echo переносим конфигурацию node-exporter
 scp -i ~/.ssh/id_rsa_root /home/anton/otusproject/wikibackprimary/node-exporter/node_exporter.service root@wikibackprimary:/etc/systemd/system/node_exporter.service
 ssh -i ~/.ssh/id_rsa_root root@wikibackprimary "systemctl daemon-reload"
 ssh -i ~/.ssh/id_rsa_root root@wikibackprimary "systemctl start node_exporter"
 ssh -i ~/.ssh/id_rsa_root root@wikibackprimary "systemctl enable node_exporter"
+
+echo устанавливаем filebeat
+scp -i ~/.ssh/id_rsa_root /home/anton/Загрузки/filebeat_8.9.1_amd64-224190-507082.deb root@wikibackprimary:/home/anton/filebeat_8.9.1_amd64.deb
+ssh -i ~/.ssh/id_rsa_root root@wikibackprimary "dpkg -i /home/anton/filebeat_8.9.1_amd64.deb"
 
 echo переносим конфигурацию filebeat
 scp -i ~/.ssh/id_rsa_root /home/anton/otusproject/wikibackprimary/filebeat/filebeat.yml root@wikibackprimary:/etc/filebeat/filebeat.yml
@@ -37,6 +65,10 @@ ssh -i ~/.ssh/id_rsa_root root@wikibackprimary "systemctl restart filebeat"
 ssh -i ~/.ssh/id_rsa_root root@wikibackprimary "systemctl enable filebeat"
 
 # Restore wikibacksecondary
+echo устанавливаем bookstack
+scp -i ~/.ssh/id_rsa_root /home/anton/otusproject/scripts/installation-ubuntu-24.04.sh root@wikibacksecondary:/home/anton/installation-ubuntu-24.04.sh
+ssh -i ~/.ssh/id_rsa_root root@wikibacksecondary "/home/anton/installation-ubuntu-24.04.sh"
+
 echo переносим конфигурацию apache
 scp -i ~/.ssh/id_rsa_root /home/anton/otusproject/wikibacksecondary/apache/bookstack.conf root@wikibacksecondary:/etc/apache2/sites-available/bookstack.conf
 ssh -i ~/.ssh/id_rsa_root root@wikibacksecondary "systemctl restart apache2"
@@ -44,14 +76,27 @@ ssh -i ~/.ssh/id_rsa_root root@wikibacksecondary "systemctl restart apache2"
 echo переносим конфигурацию bookstack
 scp -i ~/.ssh/id_rsa_root /home/anton/otusproject/wikibacksecondary/bookstack/.env root@wikibacksecondary:/var/www/bookstack/.env
 
+echo устанавливаем node-exporter
+ssh -i ~/.ssh/id_rsa_root root@wikibacksecondary "curl -LO https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz"
+ssh -i ~/.ssh/id_rsa_root root@wikibacksecondary "tar xzvf node_exporter-*.t*gz"
+ssh -i ~/.ssh/id_rsa_root root@wikibacksecondary "useradd --no-create-home --shell /bin/false node_exporter"
+ssh -i ~/.ssh/id_rsa_root root@wikibacksecondary "cp -r node_exporter-*.linux-amd64/node_exporter /usr/local/bin"
+ssh -i ~/.ssh/id_rsa_root root@wikibacksecondary "chown node_exporter: /usr/local/bin/node_exporter"
+
 echo переносим конфигурацию node-exporter
 scp -i ~/.ssh/id_rsa_root /home/anton/otusproject/wikibacksecondary/node-exporter/node_exporter.service root@wikibacksecondary:/etc/systemd/system/node_exporter.service
 ssh -i ~/.ssh/id_rsa_root root@wikibacksecondary "systemctl daemon-reload"
 ssh -i ~/.ssh/id_rsa_root root@wikibacksecondary "systemctl start node_exporter"
 ssh -i ~/.ssh/id_rsa_root root@wikibacksecondary "systemctl enable node_exporter"
 
-# Restore wikisqlmaster, wikisqlslave
-echo восстанавливаем конфигурацию sqlmaster
+Restore wikisqlmaster, wikisqlslave
+echo устанавливаем mysql на wikisqlmaster
+ssh -i ~/.ssh/id_rsa_root root@wikisqlmaster "apt install -y mysql-server-8.0"
+
+echo устанавливаем mysql на wikisqlslave
+ssh -i ~/.ssh/id_rsa_root root@wikisqlslave "apt install -y mysql-server-8.0"
+
+echo восстанавливаем конфигурацию wikisqlmaster
 scp -i ~/.ssh/id_rsa_root /home/anton/otusproject/wikisqlmaster/mysql/mysqld.cnf root@wikisqlmaster:/etc/mysql/mysql.conf.d/mysqld.cnf
 ssh -i ~/.ssh/id_rsa_root root@wikisqlmaster "systemctl restart mysql"
 ssh -i ~/.ssh/id_rsa_root root@wikisqlmaster <<'ENDSSH'
@@ -62,7 +107,7 @@ mysql -u root --execute="FLUSH TABLES WITH READ LOCK;"
 
 ENDSSH
 
-echo восстанавливаем конфигурацию sqlslaave
+echo восстанавливаем конфигурацию wikisqlslave
 scp -i ~/.ssh/id_rsa_root /home/anton/otusproject/wikisqlslave/mysql/mysqld.cnf root@wikisqlslave:/etc/mysql/mysql.conf.d/mysqld.cnf
 ssh -i ~/.ssh/id_rsa_root root@wikisqlslave "systemctl restart mysql"
 ssh -i ~/.ssh/id_rsa_root root@wikisqlslave <<'ENDSSH'
@@ -75,7 +120,12 @@ ENDSSH
 
 ssh -i ~/.ssh/id_rsa_root root@wikisqlslave "systemctl restart mysql"
 
-
+echo устанавливаем node-exporter
+ssh -i ~/.ssh/id_rsa_root root@wikisqlmaster "curl -LO https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz"
+ssh -i ~/.ssh/id_rsa_root root@wikisqlmaster "tar xzvf node_exporter-*.t*gz"
+ssh -i ~/.ssh/id_rsa_root root@wikisqlmaster "useradd --no-create-home --shell /bin/false node_exporter"
+ssh -i ~/.ssh/id_rsa_root root@wikisqlmaster "cp node_exporter-*.linux-amd64/node_exporter /usr/local/bin/node_exporter"
+ssh -i ~/.ssh/id_rsa_root root@wikisqlmaster "chown node_exporter: /usr/local/bin/node_exporter"
 
 echo переносим конфигурацию node-exporter
 scp -i ~/.ssh/id_rsa_root /home/anton/otusproject/wikisqlmaster/node-exporter/node_exporter.service root@wikisqlmaster:/etc/systemd/system/node_exporter.service
@@ -83,12 +133,22 @@ ssh -i ~/.ssh/id_rsa_root root@wikisqlmaster "systemctl daemon-reload"
 ssh -i ~/.ssh/id_rsa_root root@wikisqlmaster "systemctl start node_exporter"
 ssh -i ~/.ssh/id_rsa_root root@wikisqlmaster "systemctl enable node_exporter"
 
+echo устанавливаем node-exporter
+ssh -i ~/.ssh/id_rsa_root root@wikisqlslave "curl -LO https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz"
+ssh -i ~/.ssh/id_rsa_root root@wikisqlslave "tar xzvf node_exporter-*.t*gz"
+ssh -i ~/.ssh/id_rsa_root root@wikisqlslave "useradd --no-create-home --shell /bin/false node_exporter"
+ssh -i ~/.ssh/id_rsa_root root@wikisqlslave "cp node_exporter-*.linux-amd64/node_exporter /usr/local/bin/node_exporter"
+ssh -i ~/.ssh/id_rsa_root root@wikisqlslave "chown node_exporter: /usr/local/bin/node_exporter"
+
 echo переносим конфигурацию node-exporter
 scp -i ~/.ssh/id_rsa_root /home/anton/otusproject/wikisqlslave/node-exporter/node_exporter.service root@wikisqlslave:/etc/systemd/system/node_exporter.service
 ssh -i ~/.ssh/id_rsa_root root@wikisqlslave "systemctl daemon-reload"
 ssh -i ~/.ssh/id_rsa_root root@wikisqlslave "systemctl start node_exporter"
 ssh -i ~/.ssh/id_rsa_root root@wikisqlslave "systemctl enable node_exporter"
 
+echo устанавливаем filebeat
+scp -i ~/.ssh/id_rsa_root /home/anton/Загрузки/filebeat_8.9.1_amd64-224190-507082.deb root@wikisqlmaster:/home/anton/filebeat_8.9.1_amd64.deb
+ssh -i ~/.ssh/id_rsa_root root@wikisqlmaster "dpkg -i /home/anton/filebeat_8.9.1_amd64.deb"
 
 echo переносим конфигурацию filebeat
 scp -i ~/.ssh/id_rsa_root /home/anton/otusproject/wikisqlmaster/filebeat/filebeat.yml root@wikisqlmaster:/etc/filebeat/filebeat.yml
@@ -122,13 +182,29 @@ mysql -u root bookstack < /home/anton/sql_backup/bookstack.backup.sql
 ENDSSH
 
 # Restore wikimonitor
+ssh -i ~/.ssh/id_rsa_root root@wikimonitor "apt update"
+
+echo устанавливаем prometheus
+ssh -i ~/.ssh/id_rsa_root root@wikimonitor "apt install -y prometheus"
+ssh -i ~/.ssh/id_rsa_root root@wikimonitor "curl -LO https://github.com/prometheus/prometheus/releases/download/v2.46.0/prometheus-2.46.0.linux-amd64.tar.gz"
+ssh -i ~/.ssh/id_rsa_root root@wikimonitor "tar xzvf prometheus-*.t*gz"
+ssh -i ~/.ssh/id_rsa_root root@wikimonitor "cp -vi prometheus-*.linux-amd64/prom{etheus,tool} /usr/local/bin"
+ssh -i ~/.ssh/id_rsa_root root@wikimonitor "cp -rvi prometheus-*.linux-amd64/{console{_libraries,s},prometheus.yml} /etc/prometheus/"
+ssh -i ~/.ssh/id_rsa_root root@wikimonitor "chown -Rv prometheus: /usr/local/bin/prom{etheus,tool} /etc/prometheus/ /var/lib/prometheus/"
 
 echo переносим конфигурацию prometheus
 scp -i ~/.ssh/id_rsa_root /home/anton/otusproject/wikimonitor/prometheus/prometheus.yml root@wikimonitor:/etc/prometheus/prometheus.yml
 scp -i ~/.ssh/id_rsa_root /home/anton/otusproject/wikimonitor/prometheus/prometheus.service root@wikimonitor:/etc/systemd/system/prometheus.service
 ssh -i ~/.ssh/id_rsa_root root@wikimonitor "systemctl daemon-reload"
 ssh -i ~/.ssh/id_rsa_root root@wikimonitor "systemctl restart prometheus"
+ssh -i ~/.ssh/id_rsa_root root@wikimonitor "systemctl enable prometheus"
 
+echo устанавливаем grafana
+scp -i ~/.ssh/id_rsa_root /home/anton/Загрузки/grafana-enterprise_11.0.0_amd64.deb root@wikimonitor:/home/anton/grafana-enterprise_11.0.0_amd64.deb
+ssh -i ~/.ssh/id_rsa_root root@wikimonitor "apt install -y musl"
+ssh -i ~/.ssh/id_rsa_root root@wikimonitor "dpkg -i /home/anton/grafana-enterprise_11.0.0_amd64.deb"
+ssh -i ~/.ssh/id_rsa_root root@wikimonitor "systemctl daemon-reload"
+ssh -i ~/.ssh/id_rsa_root root@wikimonitor "systemctl start grafana-server"
 
 # # Restore wikilogs
 # echo переносим конфигурацию java
